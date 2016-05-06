25e3d94b701cac7f2d0ace36e7727ac6c26c0f48
philipgriffin <philipgriffin88@gmail.com>
2016-05-06 23:20:37 +0100

    Refactored mainApp.js

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,24 +1,12 @@
-var app = angular.module('toDoApp', []);
-app.controller('MainController', ['$scope', MainController]);
+angular.module('toDoApp', [])
+.controller('MainController', ['$scope', MainController]);
 
-app.directive('initFocus', function() {
-        var timer;
-
-        return function(scope, elm, attr) {
-            if (timer) clearTimeout(timer);
-
-            timer = setTimeout(function() {
-                elm.focus();
-                console.log('focus', elm);
-            }, 0);
-        };
-    });
 
 function MainController($scope) {
-  $scope.list = {
-  listTitle: "My List",
-  todos : [
-  ]};
+    $scope.list = {
+    listTitle: "My List",
+    todos : [
+    ]};
 
     $scope.addTodo = function() {
     var newId = $scope.list.todos.length;
@@ -27,11 +15,5 @@ function MainController($scope) {
               text: 'Type stuff here',
               completed: 0
         });
-         $('#todoInput2').focus();
-
-
-
-         console.log(newId);
-         //Materialize.toast('New task added!', 2000) // 4000 is the duration of the toast
     }
  }
